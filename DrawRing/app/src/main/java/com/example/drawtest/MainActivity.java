6f1b87627de0b15cdbfdde7a9b1a30c171bc1cd3
nnorius <128853412+nnorius@users.noreply.github.com>
2023-09-10 13:23:12 -0500

    Add files via upload

---
+++ DrawRing/app/src/main/java/com/example/drawtest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.drawtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
